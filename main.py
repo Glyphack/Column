8b9e21ec846d1a5088ca14de3b36af3682aa9924
glyphack <sh.hooshyari@gmail.com>
2019-01-19 16:40:34 +0330

    added lost condition

--- main.py
+++ main.py
@@ -175,10 +175,21 @@ def convert_shape_format(shape):
 def valid_space(shape, grid):
     accepted_pos = [[(j, i) for j in range(10)] for i in range(20)]
     accepted_pos = [j for sub in accepted_pos for j in sub]
-    
+
+    formatted = convert_shape_format(shape)
+    for position in formatted:
+        if position not in accepted_pos:
+            if position[1] > -1:
+                return False
+    return True
 
 def check_lost(positions):
-    pass
+    for pos in positions:
+        x, y = pos
+        if y < 1:
+            return True
+
+    return False
 
 def get_shape(choice_list):
     return Piece(0, 5, random.choice(shapes))

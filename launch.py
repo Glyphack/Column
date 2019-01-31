3d25173a00018d632719c441484f332a1b9b3ce9
glyphack <sh.hooshyari@gmail.com>
2019-01-31 17:46:26 +0330

    main menu is now in launch.py

---
+++ launch.py
@@ -0,0 +1,25 @@
+import pygame
+
+from main import main
+from utils import draw_text_middle
+from config import s_width, s_height
+
+
+def main_menu(win):
+    run = True
+    while run:
+        win.fill((0,0,0))
+        draw_text_middle(win, 'Press Any Key To Play', 60, (255,255,255))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                main(win)
+
+    pygame.display.quit()
+
+
+win = pygame.display.set_mode((s_width, s_height))
+pygame.display.set_caption('Column')
+main_menu(win)
